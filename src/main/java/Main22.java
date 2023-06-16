eecf2aa3e33055e1c162899e7a0dc051ab9a778f
SimreAniri <simre_aniri@bk.ru>
2023-06-17 00:55:40 +0300

    Задание 4

--- src/main/java/Main22.java
+++ src/main/java/Main22.java
@@ -28,6 +28,22 @@ public class Main22 {
         System.out.println(Arrays.toString(negToSumInd(ar2)));
         System.out.println("----".repeat(10));
 
+        Character[][] sudokuArr = {{'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+                {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+                {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+                {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+                {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+                {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+                {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+                {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+                {'.', '.', '.', '.', '8', '.', '.', '7', '9'}};
+
+        for (Character[] ar3 : sudokuArr) {
+            System.out.println(Arrays.toString(ar3));
+        }
+
+        System.out.println("Валидация: " + isValidSudoku(sudokuArr));
+
     }
 
 
@@ -95,5 +111,93 @@ public class Main22 {
             array[i] = sumInd;
         }
         return array;
+    }
+
+    /**
+     * @param ar матрица судоку
+     * @return true/false
+     * @apiNote Проверка размера сетки матрицы
+     */
+    public static boolean chekSize(Character[][] ar) {
+
+        if (ar.length != 9) return false;
+
+        for (Character[] arLine : ar) {
+            if (arLine.length != 9) return false;
+        }
+
+        return true;
+    }
+
+
+    /**
+     * @param ar матрица судоку
+     * @return true/false
+     * @apiNote Проверка правильности заполнения символами
+     */
+    public static boolean chekNum(Character[][] ar) {
+        String standart = ".123456789";
+
+        for (Character[] arLine : ar) {
+            for (Character s : arLine) {
+                if (!standart.contains(Character.toString(s))) return false;
+            }
+        }
+        return true;
+    }
+
+
+    /**
+     * @param ar матрица судоку
+     * @return true/false
+     * @apiNote Проверка отсутствия повторений
+     */
+    public static boolean chekRep(Character[][] ar) {
+        for (Character[] arLine : ar) { // проверка по строкам
+            String chekStr = "";
+            for (Character s : arLine) {
+                if (s != '.' && !chekStr.contains(Character.toString(s))) chekStr += s;
+
+                else if (chekStr.contains(Character.toString(s))) return false;
+            }
+        }
+
+        for (int i = 0; i < 9; i++) { // проверка по столбцам
+            String chekStr = "";
+            for (int j = 0; j < 9; j++) {
+                if (ar[j][i] != '.' && !chekStr.contains(Character.toString(ar[j][i]))) chekStr += ar[j][i];
+                else if (chekStr.contains(Character.toString(ar[j][i]))) return false;
+            }
+
+        }
+
+        for (int i = 0; i < 9; i += 3) { // проверка по блокам
+            for (int j = 0; j < 9; j += 3) {
+                String chekStr = "";
+                for (int k = i; k < i + 3; k++) {
+                    for (int d = j; d < j + 3; d++) {
+                        if (ar[k][d] != '.' && !chekStr.contains(Character.toString(ar[k][d]))) {
+                            chekStr += ar[k][d];
+                        } else if (chekStr.contains(Character.toString(ar[k][d]))) {
+                            System.out.println(k + " " + d);
+                            System.out.println(ar[k][d]);
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * @param ar матрица судоку
+     * @return true/false
+     * @apiNote Проверка судоку на корректность
+     */
+    public static boolean isValidSudoku(Character[][] ar) {
+        if (chekSize(ar) && chekNum(ar) && chekRep(ar)) return true;
+        else return false;
     }
 }
