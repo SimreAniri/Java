2a18fa5698c4de4d80ca37f95b9b9e58c44418ce
SimreAniri <simre_aniri@bk.ru>
2023-07-04 17:22:02 +0300

    Задание 2

--- src/main/java/Main5.java
+++ src/main/java/Main5.java
@@ -1,6 +1,4 @@
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main5 {
     public static void main(String[] args) {
@@ -8,19 +6,70 @@ public class Main5 {
 
         Scanner sc = new Scanner(System.in);
         String word = sc.nextLine().toLowerCase();
-
-
+        sc.close();
 
         System.out.println(word + " - " + countWord(word, s));
+        System.out.println("-".repeat(50));
+
+        String[] nameList = new String[]{"Иван Иванов", "Светлана Петрова", "Кристина Белова",
+                "Анна Мусина", "Анна Крутова", "Иван Юрин", "Петр Лыков", "Павел Чернов",
+                "Петр Чернышов", "Мария Федорова", "Марина Светлова", "Мария Савина", "Мария Рыкова",
+                "Марина Лугова", "Анна Владимирова", "Иван Мечников", "Петр Петин", "Иван Ежов"};
+        HashMap<String, Integer> nameCount = new HashMap<>();
+        HashMap<Integer, String> nameId = new HashMap<>();
+
+        int i = 0;
+
+        for (String str : nameList) {
+            String name = str.split(" ")[0];
+            if (!nameCount.containsKey(name)) {
+                nameCount.put(name, 1);
+                nameId.put(i, name);
+                i++;
+            }
+            else nameCount.put(name, nameCount.get(name) + 1);
+        }
+
+        int[] idList= new int[nameId.size()];
+        for (int j = 0; j < nameId.size(); j++) {
+            idList[j] = j;
+        }
+
+        System.out.println("nameCount = " + nameCount);
+
+        int temp;
+
+        for (int j = 0; j < nameCount.size(); j++) {
+
+            boolean rep = false;
+
+            for (int k = nameCount.size() - 1; k > j; k--) {
+
+                if (nameCount.get(nameId.get(idList[k])) > nameCount.get(nameId.get(idList[k - 1]))) {
+                    temp = idList[k - 1];
+                    idList[k - 1] = idList[k];
+                    idList[k] = temp;
+                    rep = true;
+                    rep = true;
+                }
+            }
+
+            if (!rep) break;
+        }
+
+        for (int id: idList) {
+            System.out.println(nameId.get(id) + " - " + nameCount.get(nameId.get(id)));
+        }
+
     }
 
     /**
-     * @apiNote Подсчитывает количество искомого слова, используя map
      * @param word слово поика
-     * @param str строка
+     * @param str  строка
      * @return количество вхождений
+     * @apiNote Подсчитывает количество искомого слова, используя map
      */
-    public static int countWord(String word, String str){
+    public static int countWord(String word, String str) {
         str = str.toLowerCase().replaceAll("\\p{Punct}|[\n]", "");
         String[] sPath = str.split(" ");
         HashMap<String, Integer> count = new HashMap<>();
