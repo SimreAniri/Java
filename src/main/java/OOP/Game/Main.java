dc31fb11a64d4ac281aeacfe64fcd19c01faa319
SimreAniri <simre_aniri@bk.ru>
2023-07-18 14:08:34 +0300

    Игра Быки-коровы

---
+++ src/main/java/OOP/Game/Main.java
@@ -0,0 +1,19 @@
+package org.example.z3.q2;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        NumberGame numberGame = new NumberGame();
+        Scanner scanner = new Scanner(System.in);
+        numberGame.start(scanner.nextInt(), scanner.nextInt());
+        scanner.nextLine();
+        while (!numberGame.getGameStatus().equals(GameStatus.FINISH)){
+            String value = scanner.nextLine();
+            Answer answer = numberGame.inputValue(value);
+          if (answer!=null){
+              System.out.println("answer = " + answer);
+          }
+        }
+    }
+}
