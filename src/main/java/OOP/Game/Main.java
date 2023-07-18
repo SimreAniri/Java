ff0add597bd326ba72e21a328ced62b9517fbfe9
SimreAniri <simre_aniri@bk.ru>
2023-07-18 18:55:10 +0300

    Реализация выбора варианта игры

--- src/main/java/OOP/Game/Main.java
+++ src/main/java/OOP/Game/Main.java
@@ -4,14 +4,42 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        NumberGame numberGame = new NumberGame();
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите длину слова и количество попыток: ");
-        numberGame.start(scanner.nextInt(), scanner.nextInt());
+
+        System.out.println("Выберите вариант игры:");
+        System.out.println("N - NumericGame");
+        System.out.println("R - RusGame");
+        System.out.println("E - EngGame");
+
+        String gameVar = scanner.nextLine().toUpperCase();
+        AbstractGame game;
+
+        switch (gameVar){
+            case "N":
+                game = new NumberGame();
+                System.out.println("Введите длину слова и количество попыток: ");
+                game.start(scanner.nextInt(), scanner.nextInt());
+                break;
+            case "E":
+                game = new EngGame();
+                System.out.println("Введите длину слова и количество попыток: ");
+                game.start(scanner.nextInt(), scanner.nextInt());
+                break;
+            case "R":
+                game = new RusGame();
+                System.out.println("Введите длину слова и количество попыток: ");
+                game.start(scanner.nextInt(), scanner.nextInt());
+                break;
+            default:
+                System.out.println("Выбран неверный вариант");
+                game = null;
+        }
         scanner.nextLine();
-        while (!numberGame.getGameStatus().equals(GameStatus.FINISH)){
+
+        while (!game.getGameStatus().equals(GameStatus.FINISH)){
+            System.out.println("Какое слово я загадал? ");
             String value = scanner.nextLine();
-            Answer answer = numberGame.inputValue(value);
+            Answer answer = game.inputValue(value);
           if (answer!=null){
               System.out.println("answer = " + answer);
           }
