920f3d86931497d0c0799f7022701eff8a5d84f4
SimreAniri <simre_aniri@bk.ru>
2023-07-25 20:47:24 +0300

    Main

--- src/main/java/OOP/S5/Main.java
+++ src/main/java/OOP/S5/Main.java
@@ -5,9 +5,9 @@ import OOP.S5.controller.Controller;
 public class Main {
     public static void main(String[] args) {
         Controller controller = new Controller();
-        controller.createStuden("qwe","asd","zxc");
-        controller.createStuden("qwe","asd","zxc");
-        controller.createStuden("qwe","asd","zxc");
+        controller.createStuden("Яков","Яковлев","А.");
+        controller.createStuden("Алексей","Романов","В.");
+        controller.createStuden("Влад","Баранов","М.");
         controller.getAllStudent();
     }
 }
