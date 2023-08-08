06457625a62311da011d33a2477cf9d23704c20c
SimreAniri <simre_aniri@bk.ru>
2023-08-08 15:02:08 +0300

    Ввод дробного числа пользователем

---
+++ src/main/java/Base/E2/Main1.java
@@ -0,0 +1,36 @@
+package Base.E2;
+
+import java.util.Scanner;
+
+public class Main1 {
+    public static void main(String[] args) {
+        float num = sayFloat();
+        System.out.println("Вы ввели: " + num);
+    }
+
+    /**
+     * @apiNote Запрашивает у пользователя ввод дробного числа
+     * @return число типа float
+     */
+    static float sayFloat() {
+        System.out.println("Введите число: ");
+        Scanner sc = new Scanner(System.in);
+
+        String temp = sc.nextLine();
+        float num;
+
+        while (!temp.matches("[0-9]+[.]?[0-9]*")) {
+            if (temp.matches("[0-9]+,?[0-9]*")) {
+                System.out.println("Число должно быть записано через точку");
+            } else {
+                System.out.println("Введено не число");
+            }
+            System.out.println("Введите число: ");
+            temp = sc.nextLine();
+        }
+        num = Float.parseFloat(temp);
+        sc.close();
+
+        return num;
+    }
+}
